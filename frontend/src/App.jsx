0d9acd7b2992f33c275bea9525245ed5b671f7d9
a-n-a-s <mirzaanas815@.com>
2021-10-04 00:12:28 +0530

    57

---
+++ frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import Home from "./Components/Home";
+import About from "./Components/About";
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+        </Switch>
+      </Router>
+    </>
+  );
+}
+
+export default App;
